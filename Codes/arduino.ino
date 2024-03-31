e7052480b6231409dd115182191fafd2ebf12c53
Queso <103338700+Queso7@users.noreply.github.com>
2024-03-31 16:37:09 -0600

    Update arduino.ino

--- Codes/arduino.ino
+++ Codes/arduino.ino
@@ -11,33 +11,33 @@ int D5 = 7;
 int D6 = 8;
 int D7 = 9;
 
-int humedad;
-int temperaturaC;
+int hum;
+int temp;
 
-DHT sensorDHT(DHTPIN, DHTTYPE);
-LiquidCrystal lcd (RS, E, D4, D5 ,D6, D7);
+DHT sensor(DHTPIN, DHTTYPE);
+LiquidCrystal lcd(RS, E, D4, D5 ,D6, D7);
 void setup() {
   Serial.begin(115200);
   lcd.begin(16, 2); 
   analogWrite(VO, 50);
-  sensorDHT.begin();
+  sensor.begin();
 }
 
 void loop() {
   delay(2000);
-  humedad = sensorDHT.readHumidity();
-  temperaturaC = sensorDHT.readTemperature();
-  if ( isnan(humedad) || isnan(temperaturaC)) {
+  hum = sensor.readHumidity();
+  temp = sensor.readTemperature();
+  if ( isnan(hum) || isnan(temp)) {
     return; 
   }           
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Temperatura: ");
-  lcd.print(temperaturaC);
+  lcd.print(temp);
   lcd.print("C");
   lcd.setCursor(0,1);
   lcd.print("Humedad: ");
-  lcd.print(humedad);
+  lcd.print(hum);
   lcd.print("%");
-  Serial.println(temperaturaC);
+  Serial.println(temp;
 }
