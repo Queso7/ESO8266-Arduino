1dbde9af5f171c5e6a702da8b2a12e7ce044031f
Queso <103338700+Queso7@users.noreply.github.com>
2024-04-25 22:11:02 -0600

    Update esp8266.ino

--- Codes/esp8266.ino
+++ Codes/esp8266.ino
@@ -1,11 +1,13 @@
-#include<time.h>
-#include<ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
+#include <WiFIClient.h>
+
 #define lv 5
 #define la 4
 #define lA 0
 #define rel 13
-time_t t;
+
 int inf, aux;
+
 void actb(){
 
 }
@@ -25,12 +27,27 @@ void inwifi(){
     digitalWrite(lv, HIGH);
     delay(200);
 }
+String get_t(){
+  WiFiClient client;
+  HTTPClient http;  
+  String timeS = "";
+  http.begin(client, "http://worldtimeapi.org/api/timezone/America/Mexico_City");
+  int httpCode = http.GET();
+  if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+    String payload = http.getString();
+    int beginS = payload.indexOf("datetime");
+    int endS = payload.indexOf("day_of_week");
+    timeS = payload.substring(beginS + 22, endS - 16);    
+  }
+  return timeS;
+}
 void instat(){ 
   digitalWrite(lv,LOW);
   digitalWrite(lA,LOW);
   digitalWrite(la,LOW);
   digitalWrite(rel,HIGH);
 }
+
 void setup(){
   Serial.begin(115200);
   pinMode(lv, OUTPUT);
@@ -40,18 +57,19 @@ void setup(){
   instat();
   inwifi();
 }
+
 void loop(){
-  time(&t);
-  inf=0;
+  String t = get_t();
+  inf = 0;
   while(Serial.available()>0){
     aux=Serial.read();
-    if(aux>=48 && aux<=57){
+    if(aux >= 48 && aux <= 57){
       inf = inf*10;
       inf = inf+(aux-48);
     }
   }
   Serial.println(inf);
   Serial.println(WiFi.localIP());
-  Serial.println(asctime(localtime(&t)));
+  Serial.println(t);
   delay(2000);
  } 
